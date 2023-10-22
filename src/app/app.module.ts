fbe664c9b57b90574943fe492dd82448bdf82013
3aziizz <abdulazeez.thajeel@gmail.com>
2023-10-22 07:00:00 +0300

    first committttt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { ViewContactComponent } from './components/view-contact/view-contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ContactManagerComponent,
+    NavbarComponent,
+    SpinnerComponent,
+    ViewContactComponent,
+    PageNotFoundComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/contact.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { IContact } from '../models/IContact';
+import { IGroup } from '../models/IGroup';
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+
+  private  serverUrl:string = `http://localhost:9000/`; //json-server URL
+  constructor(private httpClient:HttpClient) {  }
+
+//GET All Contacts
+  public getAllContacts():Observable<IContact[]> {
+    let dataURL:string = `${this.serverUrl}/contacts`;
+    return this.httpClient.get<IContact[]>(dataURL).pipe(catchError(this.handleError));
+  }
+
+//Get Single Contact
+
+public getContact(contactId:string):Observable<IContact> {
+  let dataURL:string = `${this.serverUrl}/contacts/${contactId}`;
+  return this.httpClient.get<IContact>(dataURL).pipe(catchError(this.handleError));
+}
+
+
+//Create New Contact
+
+public CreateContact(contact:IContact):Observable<IContact> {
+  let dataURL:string = `${this.serverUrl}/contacts`;
+  return this.httpClient.post<IContact>(dataURL,contact).pipe(catchError(this.handleError));
+}
+
+//Update a Contact
+
+public UpdateContact(contactId:string,contact:IContact):Observable<IContact> {
+  let dataURL:string = `${this.serverUrl}/contacts/${contactId}`;
+  return this.httpClient.put<IContact>(dataURL,contact).pipe(catchError(this.handleError));
+}
+
+//delete a Contact
+
+public DeleteContact(contactId:string):Observable<{}> {
+  let dataURL:string = `${this.serverUrl}/contacts/${contactId}`;
+  return this.httpClient.delete<IContact>(dataURL).pipe(catchError(this.handleError));
+}
+
+
+
+//GET All Groups
+public getAllGroups():Observable<IGroup[]> {
+  let dataURL:string = `${this.serverUrl}/groups`;
+  return this.httpClient.get<IGroup[]>(dataURL).pipe(catchError(this.handleError));
+}
+
+//Get Single Group
+
+public getGroup(groupId:string):Observable<IGroup> {
+let dataURL:string = `${this.serverUrl}/groups/${groupId}`;
+return this.httpClient.get<IGroup>(dataURL).pipe(catchError(this.handleError));
+}
+
+
+
+//------------------------
+
+
+  //Error handlling
+  public handleError(error:HttpErrorResponse){
+
+let errorMessage:string = '';
+if(error.error instanceof ErrorEvent){
+  //client Error
+  errorMessage = `Error : ${error.error.message}`
+}
+
+else{
+
+  //server error
+
+  errorMessage = `status : ${error.status} \n Message: ${error.message}`;
+}
+
+return throwError(errorMessage)
+  }
+
+}
